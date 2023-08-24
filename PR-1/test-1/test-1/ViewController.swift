6d2e34fb1d74cea63350630a22b3f38a2b0b3417
jusong <jusong23@naver.com>
2023-08-24 09:17:44 +0900

    Folder Categorizing

---
+++ PR-1/test-1/test-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test-1
+//
+//  Created by jusong on 2023/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
